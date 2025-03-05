6ad5522d9c209c1c0459adba163813a1fc1cb599
jonas-soderholm <jonas.soderholm89@gmail.com>
2025-03-05 21:52:02 +0100

    release fixes

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -32,18 +32,17 @@ import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export const createClient = async () => {
-  const cookieStore = await cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return cookieStore.getAll();
+        async getAll() {
+          return (await cookies()).getAll();
         },
-        setAll(cookiesToSet) {
+        async setAll(cookiesToSet) {
           try {
+            const cookieStore = await cookies(); // Fetch cookies instance properly
             cookiesToSet.forEach(({ name, value, options }) => {
               cookieStore.set(name, value, options);
             });
