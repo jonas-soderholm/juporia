25806131a0111b97da03404249d89907babacd9d
jonas-soderholm <jonas.soderholm89@gmail.com>
2026-04-13 16:28:23 +0200

    chore: add additional allowed host for middleware to support new project deployment

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,12 @@ import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
   // Update the allowed hosts to include both juporia.com and www.juporia.com
-  const allowedHosts = new Set(["localhost", "juporia.com", "www.juporia.com"]);
-
+  const allowedHosts = new Set([
+    "localhost",
+    "juporia.com",
+    "www.juporia.com",
+    "juporia-j131313-h09rt816b-jonas-soderholms-projects.vercel.app"
+  ]);
   // Get the real host from request headers (ensures Heroku doesn’t bypass the check)
   const requestHost = request.headers.get("host")?.split(":")[0]; // Strips port if present
 
